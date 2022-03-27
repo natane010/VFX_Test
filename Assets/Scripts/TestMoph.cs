e7eb85337ba8fa128e986890ef3d0476f8557044
toya_kazuya <mimikaumi@gmail.com>
2022-03-28 01:05:20 +0900

    [fix]VFX

--- Assets/Scripts/TestMoph.cs
+++ Assets/Scripts/TestMoph.cs
@@ -7,52 +7,88 @@ using UnityEngine.InputSystem;
 public class TestMoph : MonoBehaviour
 {
     [SerializeField] VisualEffect _visualEffect;
-    [SerializeField] SkinnedMeshRenderer[] _skinnedMeshRenderers;
-    [SerializeField] string sourceName;
-    [SerializeField, Range(0, 1)] int change; 
-    Vector3 orgin;
-    int Count;
+    //[SerializeField] SkinnedMeshRenderer[] _skinnedMeshRenderers;
+    [SerializeField] string changesourceName;
+    [SerializeField] string noiseName = "Noise";
+    [SerializeField, Range(0, 300)] float noiseField;
+    [SerializeField] bool change; 
+    //モデルの座標がずれているためその分だけ調整するための変数
+    Vector3 origin;
+    Vector3 norigin;
+    float Count;
     // Start is called before the first frame update
     void Start()
     {
-        orgin = transform.position;
+        //モデルのずれを無理やり直す
+        origin = transform.position;
         Count = 0;
-        if (_skinnedMeshRenderers[0] != null)
-        _visualEffect.SetSkinnedMeshRenderer(sourceName, _skinnedMeshRenderers[Count]);
-        if (Count == 0)
-        {
-            transform.position -= new Vector3(0, 1, 0);
-        }
-        else
-        {
-            transform.position += new Vector3(0, 1, 0);
-        }
+        noiseField = 0;
+        norigin = origin - new Vector3(0, 1, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Count = change;
-        InputsChange();
+        if (change)
+        {
+            ChangeForm();
+        }
+    }
+    /// <summary>
+    /// フォームの変更
+    /// </summary>
+    public void ChangeForm()
+    {
+        StartCoroutine(Transforming());
     }
 
-    public void InputsChange()
+    IEnumerator Transforming()
     {
-        //Count++;
-        //Count = Count % _skinnedMeshRenderers.Length;
-        if (Count == 0)
+        change = false;
+        yield return StartCoroutine(StartNoise());
+        if (Count == 1)
         {
-            Vector3 a = transform.position;
-            a.y = 0;
-            transform.position = a;
+            Count = 0;
         }
         else
         {
-            Vector3 a = transform.position;
-            a.y = 1;
-            transform.position = a;
+            Count = 1;
         }
-        if (_skinnedMeshRenderers[0] != null)
-            _visualEffect.SetSkinnedMeshRenderer(sourceName, _skinnedMeshRenderers[Count]);
+        //とりあえずy軸ずれを無理やり修正
+        if (Count == 0)
+        {
+            transform.position = new Vector3(transform.position.x, norigin.y, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, origin.y, transform.position.z);
+        }
+        Debug.Log("s");
+        _visualEffect.SetFloat(changesourceName, Count);
+        yield return StartCoroutine(EndNoise());
     }
+
+    IEnumerator StartNoise()
+    {
+        while(noiseField <= 300)
+        {
+            yield return null;
+            noiseField++;
+            _visualEffect.SetFloat(noiseName, noiseField);
+        }
+        yield return null;
+    }
+
+    IEnumerator EndNoise()
+    {
+        while(noiseField > 0)
+        {
+            yield return null;
+            noiseField--;
+            _visualEffect.SetFloat(noiseName, noiseField);
+        }
+        yield return null;
+    }
+    
+    
 }
